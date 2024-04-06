d0af6a412325fad6114fb8c557876b74667e8947
MrDVolk <37294024+MrDVolk@users.noreply.github.com>
2024-04-06 18:39:05 +0300

    Fixed coordinates correction

--- src/utils.py
+++ src/utils.py
@@ -5,13 +5,22 @@ scale_map = {
     'height': 40
 }
 
+correction_map = {
+    'x': 12,
+    'y': 12,
+    'width': 0,
+    'height': 0
+}
+
 def convert_value(value: str, name: str) -> int:
     value = float(value)
     if name in scale_map:
         value *= scale_map[name]
+
+    value = round_to_base(int(value))
+    correction = correction_map.get(name, 0)
     
-    value = int(value)
-    return round_to_base(value) + 12  # required for the diagram to be displayed correctly
+    return value + correction
     
 
 def round_to_base(int_number: int, base: int = 48) -> int:
